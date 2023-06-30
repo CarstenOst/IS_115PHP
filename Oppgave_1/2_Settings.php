a40adc54c82c4ec983f32c3b77eaea209011ae27
Carsten Østergaard <carsteno@uia.no>
2023-06-30 08:50:22 +0200

    Task 2_Settings.php completed (with comments explaining output)

---
+++ Oppgave_1/2_Settings.php
@@ -0,0 +1,22 @@
+<?php phpinfo();
+
+# phpinfo() is good to know to for the following:
+# PHP configuration - It displays information about PHP modules, extensions, directives and various settings.
+# version information - Make sure you are up-to-date with the php installation
+# environment details - info about the server, such as os, software and loaded modules. Good for finding compatibility issues
+# security - Helps you identify potential security risks
+# debugging and development - As it shows insight into the php setup
+# server administration - more relevant with multiple environments
+
+
+# Display errors (if script is run without server)
+# display_errors => STDOUT => STDOUT
+
+# Display errors (if script is run with server (PhpStorm))
+# Display errors is "ON" in both local and master value
+
+# Document root
+# "The document root directory under which the current script is executing, as defined in the server's configuration file." (https://www.php.net/manual/en/reserved.variables.server.php)
+# $_SERVER['DOCUMENT_ROOT'] => C:\Users\carsten\PhpstormProjects\IS_115PHP
+
+
