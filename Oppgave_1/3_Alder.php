9535ba59b6a609c458ef4a16594f3723b97f006f
Carsten Østergaard <carsteno@uia.no>
2023-06-30 21:46:38 +0200

    Reduced code in 3_Alder.php

--- Oppgave_1/3_Alder.php
+++ Oppgave_1/3_Alder.php
@@ -3,50 +3,28 @@ $navn = "Carsten";
 $alder = 23;
 ?>
 
-<!DOCTYPE html>
-<html lang="no">
-<head>
-    <title>Navn og Alder</title>
-    <style>
-        .center {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            text-align: center;
-        }
-        .left-align {
-            text-align: left;
-        }
-    </style>
-</head>
-<body>
-    <div class="center">
-        <table>
-          <tr class="left-align"><!-- tabell -->
-            <th>Navn</th>
-            <th>Alder</th>
-          </tr>
-          <tr class="left-align">
-            <td><?php echo $navn; ?></td>
-            <td><?php echo $alder; ?></td>
-          </tr>
-        </table>
+<style>.left-align { text-align: left;}</style>
+<table>
+    <tr class="left-align"><!-- tabell -->
+        <th>Navn</th>
+        <th>Alder</th>
+    </tr>
+    <tr class="left-align">
+        <td><?php echo $navn; ?></td>
+        <td><?php echo $alder; ?></td>
+    </tr>
+</table>
 
-        <ol class="left-align"><!-- nummerert liste -->
-            <li>Navn: <?php echo $navn; ?></li>
-            <li>Alder: <?php echo $alder; ?></li>
-        </ol>
+<ol class="left-align"><!-- nummerert liste -->
+    <li>Navn: <?php echo $navn; ?></li>
+    <li>Alder: <?php echo $alder; ?></li>
+</ol>
 
-        <ul class="left-align"> <!-- punktmerket liste (unordered list) -->
-            <li>Navn: <?php echo $navn; ?></li>
-            <li>Alder: <?php echo $alder; ?></li>
-        </ul>
+<ul class="left-align"> <!-- punktmerket liste (unordered list) -->
+    <li>Navn: <?php echo $navn; ?></li>
+    <li>Alder: <?php echo $alder; ?></li>
+</ul>
 
-        <p>
-            <?php echo $navn; ?> er <?php echo $alder; ?> år gammel.
-        </p>
-    </div>
-</body>
-</html>
+<p> <!-- Utskrift av navn och alder -->
+    <?php echo $navn; ?> er <?php echo $alder; ?> år gammel.
+</p>
