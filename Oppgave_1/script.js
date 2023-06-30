983cb194913f1d2e4b9752955d07bb1263761da2
Carsten Østergaard <carsteno@uia.no>
2023-06-30 21:57:09 +0200

    Some extra JS to load 2_Settings.php, 3_Alder.php and 5_Hilsen.php

---
+++ Oppgave_1/script.js
@@ -0,0 +1,13 @@
+function loadContent(file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
+
