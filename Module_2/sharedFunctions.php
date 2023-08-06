9448e72c201484e1c15e0af01275ea9e447b8c80
Carsten Østergaard <carsteno@uia.no>
2023-08-06 10:20:22 +0200

    + comments and better logic + Documentation

--- Module_2/sharedFunctions.php
+++ Module_2/sharedFunctions.php
@@ -1,27 +1,68 @@
 <?php
 
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) {
+    header('Location: index.php');
+    exit;
+}
+
 class sharedFunctions
 {
-    private static function getBaseURL(): string
+    /**
+     * Returns the base URL of any folder, in this case only used for modules
+     * @param string $folder
+     * @return string
+     */
+    public static function getBaseURL(string $folder): string
     {
+        // You can call this the current server script filepath, if URI is greek for you
         $requestURI = $_SERVER['REQUEST_URI'] ?? '/';
         $httpHost = $_SERVER['HTTP_HOST'] ?? 'localhost';
-        $modulePos = strpos($requestURI, 'Module_2');
-        return 'http://' . $httpHost . substr($requestURI, 0, $modulePos) . 'Module_2/';
+        // Here I build the base url.
+        // Perhaps the most exciting part is the strstr() which finds the needle in the haystack,
+        // where the first parameter is the haystack, second is the needle to find, and the last bool
+        // decides weather or not to return the part of the haystack before the needle, which I want in this instance.
+        // I do this because of my file structure and that I use phpStorm for quick and easy local hosting of the website.
+        // See the code on GitHub for info about the structure.
+        return 'http://' . $httpHost . strstr($requestURI, $folder, true) . $folder . '/';
     }
 
+
+    /**
+     * Gets all directories with a controller from current dir
+     * @param string $currentDir The current directory or the root folder you want to search
+     * @return array Returns all directories that has a controller based on root folder
+     */
     private static function getDirectoriesWithController(string $currentDir): array
     {
-        $directories = scandir($currentDir);
+        $directories = scandir($currentDir, SCANDIR_SORT_NONE);
         return array_filter($directories, function($dir) use ($currentDir) {
-            return $dir != '.' && $dir != '..' && is_dir($currentDir . '/' . $dir) && file_exists($currentDir . '/' . $dir . '/Controller.php');
+            return is_dir($currentDir . '/' . $dir) && file_exists($currentDir . '/' . $dir . '/Controller.php');
         });
     }
 
-    public static function generateButtons(): void
+    /**
+     * Generates navigation buttons for directories that have a 'Controller.php' file.
+     *
+     * This method retrieves all directories in the current directory that contain a 'Controller.php' file.
+     * It then generates HTML buttons for each of these directories, using the directory name as the button label.
+     * These buttons, when clicked, will navigate to the 'Controller.php' file in the corresponding directory.
+     *
+     * @param string $folder The name of the folder that will be appended to the base URL for generating the button links.
+     *
+     * @return void This method does not return anything. It directly echoes the HTML buttons to the output.
+     *
+     * @throws Exception If any errors occur during directory scanning or URL generation, an Exception may be thrown.
+     *
+     * Usage:
+     *
+     * <?php
+     * sharedFunctions::generateNavigationButtons('MyFolder');
+     * ?>
+     */
+    public static function generateNavigationButtons(string $folder): void
     {
         $currentDir = __DIR__;
-        $baseURL = self::getBaseURL();
+        $baseURL = self::getBaseURL($folder);
         $directories = self::getDirectoriesWithController($currentDir);
 
         foreach ($directories as $dir) {
