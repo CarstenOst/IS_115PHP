e180787cffbce60811954b9e6830effd35a730c0
Carsten Østergaard <carsteno@uia.no>
2023-08-10 20:50:40 +0200

    PostHandler.php comments

--- Module_2/PostHandler.php
+++ Module_2/PostHandler.php
@@ -2,17 +2,26 @@
 
 class PostHandler
 {
-    // If the submit button is pressed, this code will run
+    /**
+     * This function returns the input from the user, if there is any
+     * @param string $cookieName The name of the cookie to get user input (note that this is not a set cookie, but from the super global $_POST)
+     * @return string
+     */
     private static function handlePostRequest(string $cookieName): string {
         return $_POST[$cookieName] ?? '';
     }
 
+    /**
+     * This handles the post request, and if there is a cookie and the button is pressed, it removes it
+     * @param string $cookieName
+     * @return string
+     */
     public static function requestPost(string $cookieName): string {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Remove existing cookie
             // Ignore the spaghetti, this is not the task anyway :)
-            if (isset($_POST['remove_cookies']) and CookieHelper::isCookie($cookieName)) {
-                CookieHelper::removeCookie($cookieName);
+            if (isset($_POST['remove_cookies']) and CookieHandler::isCookie($cookieName)) {
+                CookieHandler::removeCookie($cookieName);
                 return '';
             }
             return self::handlePostRequest($cookieName);
@@ -23,14 +32,24 @@ class PostHandler
 
     // For task 2
     // The only difference is that I remove the php code from the input as soon as possible
+    /**
+     * This function returns the input from the user stripped of php and html code, if there is any
+     * @param string $cookieName The name of the cookie to get user input
+     * @return string
+     */
     private static function secureHandleLastNamePostRequest(string $cookieName): string {
         return RemoveCode::removePhpCode($_POST[$cookieName]) ?? '';
     }
 
+    /**
+     * This handles the post request, and if there is a cookie and the button is pressed, it removes it
+     * @param string $cookieName
+     * @return string without php or html code
+     */
     public static function secureRequestPost(string $cookieName): string {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_POST['remove_cookies']) and CookieHelper::isCookie($cookieName)) {
-                CookieHelper::removeCookie($cookieName);
+            if (isset($_POST['remove_cookies']) and CookieHandler::isCookie($cookieName)) {
+                CookieHandler::removeCookie($cookieName);
                 return '';
             }
             return self::SecureHandleLastNamePostRequest($cookieName);
