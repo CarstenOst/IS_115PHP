a6da2d290aad633b0b3dc218279dd5869db4aa20
Carsten Østergaard <carsteno@uia.no>
2023-08-11 12:03:27 +0200

    Comments

--- Module_2/Task 5/PasswordGenerator.php
+++ Module_2/Task 5/PasswordGenerator.php
@@ -3,7 +3,12 @@
 class PasswordGenerator
 {
     // Random, with minimum 8 characters. At least one number, and one upper (and lower) case letter.
-    private static function passwordGenerator($length = 8): string {
+    /**
+     * Generate a password with standard length of 8 characters
+     * @param int $length The length of the password
+     * @return string The generated password
+     */
+    private static function passwordGenerator(int $length = 8): string {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+[]{}|;:,.<>?';
         $password = '';
         // We need to know the max length of the characters string as to not try to look up an index that does not exist
@@ -21,31 +26,40 @@ class PasswordGenerator
         return $password;
     }
 
+    /**
+     * Checks if the password is valid
+     * @param string $password The password to check
+     * @return bool true if the password is valid, false if the password is invalid
+     */
     private static function isValid(string $password): bool {
         // check if the password is at least 8 characters long
         if (strlen($password) < 8) {
             return false;
         }
 
-        // check if the password contains at least one number
+        // check if the password !contains at least one number
         if (!preg_match('/[0-9]/', $password)) {
             return false;
         }
 
-        // check if the password contains at least one upper case letter
+        // check if the password !contains at least one upper case letter
         if (!preg_match('/[A-Z]/', $password)) {
             return false;
         }
 
-        // check if the password contains at least one lower case letter
+        // check if the password !contains at least one lower case letter
         if (!preg_match('/[a-z]/', $password)) {
             return false;
         }
-
+        // If all checks pass, return true
         return true;
     }
 
-
+    /**
+     * Get a valid password
+     * @param int $length The length of the wanted password
+     * @return string The generated password
+     */
     public static function getValidPassword(int $length = 8): string {
         $maxAttempts = 100;
         do {
