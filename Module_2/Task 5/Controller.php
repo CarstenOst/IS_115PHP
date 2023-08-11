77718d693d15ee9987dcad5d296bed14ec86feae
Carsten Østergaard <carsteno@uia.no>
2023-08-11 21:37:09 +0200

    Comments

--- Module_2/Task 5/Controller.php
+++ Module_2/Task 5/Controller.php
@@ -3,7 +3,7 @@ require 'PasswordGenerator.php';
 require_once '../sharedViewTop.php';
 
 
-
+// Display the new password button
 echo <<<HTML
     <form method="post">
         <button type="submit" name="refresh">New password</button>
@@ -11,19 +11,19 @@ echo <<<HTML
     <br>
 HTML;
 
+// Get a valid password
 $password = PasswordGenerator::getValidPassword();
 
+// Display the password.
+// Without the htmlspecialchars() function, the password will sometimes not be displayed correctly.
+echo 'Your randomly generated password is: ' . htmlspecialchars($password);
 
-echo 'Your randomly generated password is: ';
-// Without htmlspecialchars(), the password will sometimes not be displayed correctly
-echo htmlspecialchars($password);
-
-
+// If the button from the displayed button is pressed, refresh the page
 if (isset($_POST['generate password'])) {
     // Refresh the page
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
 }
 
-
+// It works without this line, but it's good practice to close the HTML document
 require_once '../sharedViewBottom.php';
