8723df6b6cff6ec32ef92226f01ba42e350a63d3
Carsten Østergaard <carsteno@uia.no>
2023-08-20 10:37:24 +0200

    mod3 setup

---
+++ Module_3/sharedViewTop.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <title>Module 2</title>
+</head>
+<body>
+<div class="center">
+    <nav id="navbar" style="align-items: center" >
+        <?php require_once 'sharedFunctions.php';sharedFunctions::generateNavigationButtons('Module_3');?>
+    </nav>
+    <div id="content" class="center" style="padding-top: 55px;">
