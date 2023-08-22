f7040ca80ff99abbc29f93bf850ad3ae788be97c
Carsten Østergaard <carsteno@uia.no>
2023-08-22 18:05:43 +0200

    Task 1 good enough

--- Module_3/Task 1/Controller.php
+++ Module_3/Task 1/Controller.php
@@ -2,20 +2,16 @@
 require 'Event.php';
 require 'DateChecker.php';
 require '../PostHandler.php';
-require '../sharedViewTop.php';
-
 
 require '../sharedViewTop.php';
 
 
-$event1 = new Event("NeonParty", "eventdescription1", "eventcontact1", "eventlocation1", "Europe/Paris", "2023-08-20 6:22:00", "PT4H");
-$event2 = new Event("Black and White", "eventdescription1", "eventcontact1", "eventlocation1", "Europe/Paris", "2023-08-20 6:22:00", "PT4H");
+$event1 = new Event("NeonParty", "eventdesc1", "eventco1", "Eidehallen", "Europe/Paris", "2023-08-27 6:22:00", "PT4H");
+$event2 = new Event("Black and White", "eventdesc2", "eventco2", "Gimlehallen", "Europe/Paris", "2023-08-20 6:22:00", "PT4H");
 
 
 function printEvent($event): void
 {
-
-
     foreach ($event as $item):
         $eventName = $item->getEventName();
         $eventDescription = $item->getEventDescription();
@@ -25,14 +21,11 @@ function printEvent($event): void
         $eventDate = $item->getEventDate()->format('d/m/Y H:i:s');
         $eventDuration = $item->getEventDuration()->format('%h hours, %i minutes');
         $eventEndDate = $item->getEventEndDate()->format('d/m/Y H:i:s');
-
-
         ?>
         <div class="suggestion-div mt-4 shadow-sm m-sm-2" style="padding-left: 5px;">
             <table class="suggestion-table">
                 <tr>
                     <td class="text-center pt-2" style="width: 60px;">
-
                     </td>
                     <td colspan="3" class="suggestion-title" style="padding-right: 6px;">
                         <?php
@@ -47,36 +40,25 @@ function printEvent($event): void
                     </td>
                 </tr>
                 <tr>
-                    <td class="text-center pb-2"><?php echo $eventContact; ?></td>
-                    <td style="text-align: left; padding-left: 5px;" class="pb-2 col-3 text-nowrap">
-                        <?php
-                        switch ($eventDescription) {
-                            case "Godkjent":
-                                // ...
-                                break;
-                            // ...
-                            default:
-                                echo '<span class="">' . $eventDescription . '</span>';
-                                break;
-                        }
-                        ?>
-                    </td>
                     <td style="text-align: center" class="pb-2 col-3"><?php echo $eventLocation; ?></td>
                     <td style="text-align: right; padding-right: 10px;" class="pb-2 col-3">
                         <span class="text-nowrap d-none d-sm-inline">
                             <i class="fa fa-calendar-plus-o" style="margin-right: 3px;" aria-hidden="true"></i>
-                            Begins <?php echo $eventDate; ?><br/>
+                            <?php echo $eventDate; ?><br/>
                         </span>
                         <?php
                         $dateToday = new DateTime();
-                        $suggestionDueDate = $item->getEventEndDate();
-                        $daysLeft = $suggestionDueDate->diff($dateToday)->days;
+                        $eventEndDate = $item->getEventEndDate();
+                        $daysLeft = $eventEndDate->diff($dateToday)->days;
                         $daysLeftRounded = round($daysLeft);
+                        if ($dateToday > $eventEndDate) {
+                            $daysLeftRounded = -$daysLeftRounded;
+                        }
 
-                        if ($daysLeftRounded <= 0) {
-                            // ...
+                        if ($daysLeftRounded >= 0) {
+                            echo $daysLeftRounded . ' days until event';
                         } else {
-                            // ...
+                            echo -$daysLeftRounded . ' days old';
                         }
                         ?>
                     </td>
@@ -94,12 +76,28 @@ $eventTitle = $event1->getEventName();
 
 
 if (DateChecker::isOldDate($event1->getEventEndDate())) {
-    echo "$eventTitle is an old event. It ended on " . $event1->getEventEndDate()->format('d/m/Y H:i:s') . ".";
-} else if (DateChecker::isOldDate($event1->getEventDate())) {
-    echo "$eventTitle is a current event";
-} else {
-    echo "$eventTitle is a future event";
+    echo "$eventTitle is an old event. <br> It ended on " . $event1->getEventEndDate()->format('d/m/Y H:i:s') . ".";
+    return;
 }
+if (DateChecker::isOldDate($event1->getEventDate())) {
+    echo "$eventTitle is a current event";
+    return;
+}
+echo "$eventTitle is a future event";
+
+echo '<br><br>';
+$eventTitle = $event2->getEventName();
+
+
+if (DateChecker::isOldDate($event2->getEventEndDate())) {
+    echo "$eventTitle is an old event. <br> It ended on " . $event2->getEventEndDate()->format('d/m/Y H:i:s') . ".";
+    return;
+}
+if (DateChecker::isOldDate($event2->getEventDate())) {
+    echo "$eventTitle is a current event";
+    return;
+}
+echo "$eventTitle is a future event";
 
 
 require '../sharedViewBottom.php';
