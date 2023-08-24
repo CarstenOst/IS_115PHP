7a280dd1c3ffcd779710bf4c9554d43ad8a6a799
Carsten Østergaard <carsteno@uia.no>
2023-08-24 18:02:12 +0200

    task 4 done

--- Module_3/Task 4/Controller.php
+++ Module_3/Task 4/Controller.php
@@ -1,13 +1,37 @@
 <?php
 require '../PostHandler.php';
+require '../SharedFormRenderer.php';
 
+CONST COMMUNE_NAME = 'Commune_Name';
 
-
-
+// Check region
 
 
 require '../sharedViewTop.php';
 
+// Associative array with commune keys, and region as value
+$communes = [
+    'Kristiansand' => 'Vest-Agder',
+    'Lillesand' => 'Vest-Agder',
+    'Birkenes' => 'Aust-Agder',
+    'Harstad' => 'Troms og Finnmark',
+    'Kvæfjord' => 'Troms og Finnmark',
+    'Tromsø' => 'Troms og Finnmark',
+    'Bergen' => 'Vestland',
+    'Trondheim' => 'Trøndelag',
+    'Bodø' => 'Nordland',
+    'Alta' => 'Troms og Finnmark'
+];
+
+SharedFormRenderer::printForm(COMMUNE_NAME, 'Enter your Norwegian commune name');
+
+$selectedCommune = PostHandler::secureRequestPost(COMMUNE_NAME); // Replace this with the desired commune
+
+
+if ($selectedCommune) {
+    $message = $communes[$selectedCommune] ?? "Ukjent kommune: $selectedCommune";
+    echo "$selectedCommune ligger i $message fylke.";
+}
 
 
 
@@ -27,6 +51,4 @@ require '../sharedViewTop.php';
 
 
 
-
-
-require '../sharedViewbottom.php';
+require '../sharedViewBottom.php';
