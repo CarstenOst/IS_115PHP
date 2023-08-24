ba1d78d30d2514048b645d459d88dbf741d76c68
Carsten Østergaard <carsteno@uia.no>
2023-08-24 18:02:36 +0200

    task 5 started, (bad logic)

--- Module_3/Task 5/Controller.php
+++ Module_3/Task 5/Controller.php
@@ -3,7 +3,7 @@ require '../PostHandler.php';
 
 
 
-
+// Chess
 
 
 require '../sharedViewTop.php';
@@ -11,6 +11,29 @@ require '../sharedViewTop.php';
 
 
 
+$grainsOnFirstSquare = 1; // Start with 1 grain on the first square
+$grainWeightGrams = 0.035; // Weight of a grain in grams
+
+$chessboardSize = 64; // Total number of squares on a chessboard
+
+$grainsOnEachSquare = [$grainsOnFirstSquare];
+$totalGrains = $grainsOnFirstSquare;
+
+// Calculate the number of grains on each square and the total grains
+for ($i = 1; $i < $chessboardSize; $i++) {
+    $grainsOnEachSquare[] = $grainsOnEachSquare[$i - 1] * 2;
+    $totalGrains += $grainsOnEachSquare[$i];
+}
+
+$weightInGrams = $totalGrains * $grainWeightGrams;
+$weightInKilograms = $weightInGrams / 1000; // Convert grams to kilograms
+$weightInTons = $weightInKilograms / 1000; // Convert kilograms to tons
+
+// Display results
+echo "Rute\tAntall hvetekorn\tVekt (tonn)<br>";
+for ($i = 0; $i < $chessboardSize; $i++) {
+    echo ($i + 1) . "\t" . $grainsOnEachSquare[$i] . "\t" . number_format($weightInTons, 4) . "<br>";
+}
 
 
 
@@ -29,4 +52,5 @@ require '../sharedViewTop.php';
 
 
 
-require '../sharedViewbottom.php';
+
+require '../sharedViewBottom.php';
