80a862442360b650d4abcc6474ef343bb2babe4c
Carsten Østergaard <carsteno@uia.no>
2023-08-24 19:50:51 +0200

    Task 5 finished Chesssss

--- Module_3/Task 5/Controller.php
+++ Module_3/Task 5/Controller.php
@@ -1,56 +1,65 @@
 <?php
 require '../PostHandler.php';
-
-
-
-// Chess
-
-
 require '../sharedViewTop.php';
 
 
+//echo "Square 1 <br>";
+//echo $totalGrains . "<br>";
+//echo "Square ". $i+1;
+//echo '<br>';
+//echo $totalGrains;
+//echo '<br>';
 
-
+// Could be made const, but whatever
 $grainsOnFirstSquare = 1; // Start with 1 grain on the first square
-$grainWeightGrams = 0.035; // Weight of a grain in grams
+$grainWeightGrams = 0.035; // Weight of a grain in grams (wikipedia says 0.065g)
 
 $chessboardSize = 64; // Total number of squares on a chessboard
 
 $grainsOnEachSquare = [$grainsOnFirstSquare];
 $totalGrains = $grainsOnFirstSquare;
 
+
 // Calculate the number of grains on each square and the total grains
+//for ($i = 1; $i < $chessboardSize; $i++) {
+//    $grainsOnEachSquare[] = $grainsOnEachSquare[$i - 1] * 2;
+//    $totalGrains += $grainsOnEachSquare[$i];
+//}
+
 for ($i = 1; $i < $chessboardSize; $i++) {
-    $grainsOnEachSquare[] = $grainsOnEachSquare[$i - 1] * 2;
-    $totalGrains += $grainsOnEachSquare[$i];
+    $grainsOnCurrentSquare = 2 ** $i;
+    $grainsOnEachSquare[] = $grainsOnCurrentSquare;
+    $totalGrains += $grainsOnCurrentSquare;
 }
 
-$weightInGrams = $totalGrains * $grainWeightGrams;
-$weightInKilograms = $weightInGrams / 1000; // Convert grams to kilograms
-$weightInTons = $weightInKilograms / 1000; // Convert kilograms to tons
 
 // Display results
-echo "Rute\tAntall hvetekorn\tVekt (tonn)<br>";
-for ($i = 0; $i < $chessboardSize; $i++) {
-    echo ($i + 1) . "\t" . $grainsOnEachSquare[$i] . "\t" . number_format($weightInTons, 4) . "<br>";
+for ($i = 0; $i < count($grainsOnEachSquare); $i++) {
+    echo "Square number:  " . ($i+1) ."<br>";
+    echo "Number of grains: " . getFormattedNumber($grainsOnEachSquare[$i]) . "<br>";
+    echo "Weight in metric tonnes: " . getWeightInTons($grainsOnEachSquare[$i], $grainWeightGrams) . "<br> <br>";
+}
+echo "Total number of grains: " . getFormattedNumber($totalGrains) . "<br>";
+echo "Total weight in metric tonnes: " . getWeightInTons($totalGrains, $grainWeightGrams) . "<br> <br>";
+
+
+function getWeightInTons($grains, $grainWeightGrams): string
+{
+    $weightInGrams = $grains * $grainWeightGrams;
+    $weightInKilograms = $weightInGrams / 1000; // Convert grams to kilograms
+    $weightInTons = $weightInKilograms / 1000; // Convert kilograms to tons
+
+    return getFormattedNumber($weightInTons);
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function getFormattedNumber($number): string
+{
+    if ($number < 1) {
+        return number_format($number, 9);
+    }
+    return number_format($number, 0);
+}
 
 
 require '../sharedViewBottom.php';
