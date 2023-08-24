9d4b35c82a363fb90ec1ef9e3e76e7960f836899
Carsten Østergaard <carsteno@uia.no>
2023-08-24 15:24:47 +0200

    Task 3 done

--- Module_3/Task 3/Controller.php
+++ Module_3/Task 3/Controller.php
@@ -6,15 +6,32 @@ CONST BALANCE_COOKIE = 'Balance';
 CONST INTEREST_COOKIE = 'Interest';
 
 
-
-
-
 require '../sharedViewTop.php';
 
-// Start balance
-$S0 = 0;
 SharedFormRenderer::printTwoInputForms(BALANCE_COOKIE, 'Enter your balance', INTEREST_COOKIE, 'Enter your interest');
-// interest
+
+$balance = floatval(PostHandler::secureRequestPost(BALANCE_COOKIE));
+$interest = floatval(PostHandler::secureRequestPost(INTEREST_COOKIE));
+
+
+
+if ($balance and $interest) {
+    if ($interest < 1) {
+        $interest += 1;
+    }
+
+    echo "Balance is $balance <br>";
+    echo "The interest is set to; $interest <br>";
+
+    for ($i = 1; $i < 10; $i++) {
+        $balance *= $interest;
+        echo "Balance after $i year is; $balance <br>";
+    }
+} else {
+    echo 'Enter int or float values only';
+}
+
+
 
 
 
