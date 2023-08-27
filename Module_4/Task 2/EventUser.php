b20372fba2326aab7bb7f68aa3693b71bbacca42
Carsten Østergaard <carsteno@uia.no>
2023-08-27 13:33:28 +0200

    + EventUser.php

---
+++ Module_4/Task 2/EventUser.php
@@ -0,0 +1,10 @@
+<?php
+
+class EventUser
+{
+    private string $name;
+    private string $phoneNumber; // Don't need math on a phone number anyway, so string is fine
+    private string $email;
+
+
+}
