46f62f14090296cef5521035deaaa23f4f3d380c
Carsten Østergaard <carsteno@uia.no>
2023-08-27 12:32:25 +0200

    Started task 1 mod4

---
+++ Module_4/Task 1/AssocArray.php
@@ -0,0 +1,57 @@
+<?php
+
+class AssocArray
+{
+    /**
+     * Warning: This function will overwrite the variable passed as first parameter
+     * Creates an array with keys from the given parameters
+     * @param $arr - array to be created; WARNING WILL OVERWRITE
+     * @param int ...$arrayKey - keys for the array
+     * @return bool - true if array was created, false if not
+     * Value will always be 1
+     */
+    public static function create(&$arr ,int ...$arrayKey): bool
+    {
+        $arr = [];
+        if (!$arrayKey) {
+            return false;
+        }
+
+        foreach ($arrayKey as $key) {
+            $arr[$key] = 1;
+        }
+        return true;
+    }
+
+
+    /**
+     * Takes a pointer to the first parameter value, and either adds or overwrites the value (if a key is the same) with the given keys
+     * Be aware of side effects
+     * @param array $arr - array to be modified
+     * @param int ...$arrayKey - keys for the array
+     * @return bool - true if array was modified, false if not
+     */
+    public static function modify(array &$arr ,int ...$arrayKey): bool
+    {
+        if (!$arrayKey) {
+            return false;
+        }
+
+        foreach ($arrayKey as $key) {
+            $arr[$key] = 1;
+        }
+        return true;
+    }
+
+    /**
+     * Prints out an assoc array with the given html code below
+     * @param array $assocArray
+     * @return void
+     */
+    public static function print(array $assocArray): void
+    {
+        foreach ($assocArray as $key => $value) {
+            echo "Key: $key, Value: $value <br>";
+        }
+    }
+}
---
+++ Module_4/Task 1/Controller.php
@@ -0,0 +1,27 @@
+<?php
+// Draw the navbar
+require '../sharedViewTop.php';
+// Include the class for this task (1)
+include 'AssocArray.php';
+
+
+
+
+// Create an associative array with the following keys (value will be 1, as the task did not specify a value);
+AssocArray::create($assocArr,0,3, 5, 7, 8, 15);
+
+// You can uncomment the following line to see the array become modified;
+//AssocArray::modify($assocArr,13,452, 545, 5, 242, 125);
+
+// Print the array with the following function:
+AssocArray::print($assocArr);
+
+
+echo '<br> Running print_r() <br>';
+// Print the array with keys and values by using print_r();
+print_r($assocArr);
+
+
+
+// Finish the html page (not actually needed)
+require '../sharedViewBottom.php';
