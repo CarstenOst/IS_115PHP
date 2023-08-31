d7fa8bc7655759eb03a460dd36396c22de9b8bd5
Carsten Østergaard <carsteno@uia.no>
2023-08-31 08:42:12 +0200

    Some InputValidation.php logic

--- Module_4/Task 2/Controller.php
+++ Module_4/Task 2/Controller.php
@@ -21,22 +21,22 @@ $name = PostHandlerM4::secureRequestPost(NAME_COOKIE);
 $email = PostHandlerM4::secureRequestPost(EMAIL_COOKIE);
 $phoneNumber = PostHandlerM4::secureRequestPost(NUMBER_COOKIE);
 
-echo $name . '<br>';
+if ($name and InputValidate::hasOnlyCharactersInAlphabet($name)) {
+    SharedHtmlRendererM4::generateResponse("$name must have letters, and no numbers or special characters", false);
+}
+
+
 if ($email) {
 
     if (InputValidate::isEmail($email)) {
         SharedHtmlRendererM4::generateResponse("$email is valid", true);
+        echo $name . '<br>';
         echo $email . '<br>';
+        echo $phoneNumber . '<br>';
         return;
     }
-    SharedHtmlRendererM4::generateResponse('Email is not valid', false);
-    SharedHtmlRendererM4::generateResponse('Email is not valid', false);
-    SharedHtmlRendererM4::generateResponse('Email is not valid', false);
-    SharedHtmlRendererM4::generateResponse('Email is not valid', false);
+    SharedHtmlRendererM4::generateResponse('Email is invalid', false);
 }
-
-
-echo $phoneNumber . '<br>';
 
 
 // Generate Response
--- Module_4/Task 2/InputValidation.php
+++ Module_4/Task 2/InputValidation.php
@@ -2,7 +2,33 @@
 
 class InputValidate
 {
-    public static function isEmail(string $email) {
+    public static function isEmail(string $email)
+    {
         return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function hasNumbers(string $str): bool
+    {
+        return preg_match('/[0-9]/', $str);
+    }
+
+    public static function hasLetters(string $str): bool
+    {
+        if (!preg_match('/[A-Z]/', strtoupper($str))) {
+            return false;
+        }
+        return true;
+    }
+
+    public static function hasOnlyCharactersInAlphabet(string $str): bool
+    {
+        if (!$str or $str == '\\') {
+            return false;
+        }
+        if (preg_match('#[^A-Z!]#', strtoupper($str))) {
+            echo $str;
+            return true;
+        }
+        return false;
     }
 }
