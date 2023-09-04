25be5944146c1a8cc38bcb15fe0b80ba11e5420a
Carsten Østergaard <carsteno@uia.no>
2023-09-04 11:49:15 +0200

    Email spam?

--- Module_4/Task 2/Controller.php
+++ Module_4/Task 2/Controller.php
@@ -21,36 +21,56 @@ $name = PostHandlerM4::secureRequestPost(NAME_COOKIE);
 $email = PostHandlerM4::secureRequestPost(EMAIL_COOKIE);
 $phoneNumber = PostHandlerM4::secureRequestPost(NUMBER_COOKIE);
 
-$notValidResponseMessage = '';
-
-if ($name and InputValidate::hasNoSpecialCharacters($name)) {
-    $notValidResponseMessage .= "$name must have letters, and no numbers or special characters <br>";
+if (!$name && !$email && !$phoneNumber) {
+    return;
 }
 
+$notValidResponseMessage = '';
 
-
+// If name exists and is not valid
+if ($name && !InputValidate::hasNoSpecialCharacters($name)) {
+    $notValidResponseMessage .= "'$name' must have letters, and no numbers or special characters <br>";
+}
 // If email exists and is not valid
 if ($email && !InputValidate::isEmail($email)) {
     $notValidResponseMessage .= "'$email' is not a valid valid email <br>";
+}
+// If phone number exists and is not valid
+if ($phoneNumber && !InputValidate::hasOnlyNumbers("123")) {
+    $notValidResponseMessage .= "'$phoneNumber' does not have only numbers <br>";
 }
 
 
 // Print the notValid response message if it exists
 if ($notValidResponseMessage) {
     SharedHtmlRendererM4::generateResponse($notValidResponseMessage, false);
-    return;
+    return; // Stop execution if any of the information is not valid
 }
 
-if($name && $email && $phoneNumber) {
-    SharedHtmlRendererM4::generateResponse("User was successfully registered <br> Name is: $name <br> Email is: $email <br> Phone number is: $phoneNumber", true);
-}
 
-if (InputValidate::hasOnlyNumbers("123")){
-    echo "true";
-} else {
-    echo "false";
-}
+SharedHtmlRendererM4::generateResponse("User was successfully registered <br> Name is: $name <br> Email is: $email <br> Phone number is: $phoneNumber", true);
 
+
+$to = $_POST[EMAIL_COOKIE];
+$subject = "Email Subject";
+
+$message = 'Dear blyat,<br>';
+$message .= "We welcome you to be part of family<br><br>";
+$message .= "Regards,<br>";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <politiet@uia.no>' . "\r\n";
+$headers .= 'Cc: slappkuk123@gmail.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+
+echo "sendt mail to $to, with subject $subject, and message $message, and headers $headers";
+
+echo "<br><br>";
 // Generate Response
 echo $name . '<br>';
 echo $email . '<br>';
--- Module_4/Task 2/InputValidation.php
+++ Module_4/Task 2/InputValidation.php
@@ -7,26 +7,14 @@ class InputValidate
         return filter_var($email, FILTER_VALIDATE_EMAIL) ?? false;
     }
 
-    public static function hasNumbers(string $str): bool
-    {
-        return preg_match('/[0-9]/', $str);
-    }
-
     public static function hasOnlyNumbers(string $str): bool
     {
-        if (!preg_match('/^[0-9\s]+$/', $str)) {
+        if (!preg_match('/^[0-9\s+]+$/', $str)) {
             return false;
         }
         return true;
     }
 
-    public static function hasLetters(string $str): bool
-    {
-        if (!preg_match('/[A-Z]/', strtoupper($str))) {
-            return false;
-        }
-        return true;
-    }
 
     /**
      * Checks if string has no special characters and only letters (numbers are discarded too)
@@ -38,7 +26,7 @@ class InputValidate
         if (!$str) {
             return false;
         }
-        if (preg_match('/[^A-ZÆØÅ ]/iu', $str)) {
+        if (!preg_match('/[^A-ZÆØÅ ]/iu', $str)) {
             echo $str;
             return true;
         }
