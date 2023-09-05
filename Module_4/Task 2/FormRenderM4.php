68e7edcb01391df2df71bd9531b902a16eb7acea
Carsten Østergaard <carsteno@uia.no>
2023-09-05 12:31:03 +0200

    + renderFormArrayBased (scalable)

--- Module_4/Task 2/FormRenderM4.php
+++ Module_4/Task 2/FormRenderM4.php
@@ -2,7 +2,16 @@
 
 class FormRenderM4
 {
-
+    /**
+     * This function is now deprecated, as it is hardcoded to always need 3 inputs of both cookienames and labeltext
+     * @param string $cookieName
+     * @param string $labelText
+     * @param string $cookieName2
+     * @param string $labelText2
+     * @param string $cookieName3
+     * @param string $labelText3
+     * @return void
+     */
     public static function renderForm(
         string $cookieName, string $labelText,
         string $cookieName2, string $labelText2,
@@ -23,5 +32,36 @@ class FormRenderM4
                 <input id="pointer" type="submit" value="Submit">
             </form>
         EOT;
+    }
+
+    /**
+     * Renders form based on array input
+     * This means that you can render as many input fields you want as long as you have enough memory
+     * @param array $cookieNames Fill in as many cookie names that you want
+     * @param array $labelText Fill inn as many labels as the amount of cookie names
+     * @return void echos the form
+     */
+    public static function renderFormArrayBased(array $cookieNames, array $labelText): void
+    {
+        if (!$cookieNames) {
+            return;
+        }
+        $form = '<form class="form-group" id="form" action="" method="POST">';
+        $i = 0;
+        foreach ($cookieNames as $cookie) {
+            $form .= <<<EOT
+                <label for="$cookie">$labelText[$i]</label><br>
+                <input type="text" name="$cookie" id="$cookie"><br>
+            EOT;
+            $i++;
+        }
+        // Concat the rest of the form, so we actually can submit our info
+        $form .= <<<EOT
+                <br>
+                <input id="pointer" type="submit" value="Submit">
+            </form>
+            EOT;
+
+        echo $form;
     }
 }
