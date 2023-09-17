6a9cce559f34708dd25708e8d2c8ae1a71a91075
Carsten Østergaard <carsten.ostergaard@sikri.no>
2023-09-17 15:32:49 +0200

    M4T3

--- Module_4/Task 3/Controller.php
+++ Module_4/Task 3/Controller.php
@@ -1,3 +1,25 @@
 <?php
 require '../sharedViewTop.php';
+
+
+/*
+    Lag et script som viser en brukerprofil basert på innholdet i en matrise. For å gjøre dette må du lage en
+    matrise med den nødvendige informasjonen i begynnelsen. Visningen skal gjøres i form av et skjema
+    som inneholder den eksisterende informasjonen om brukeren. Dersom skjemaet sendes, skal den nye
+    informasjonen lagres i matrisen. Løsningen er ekstra elegant dersom du først sjekker om det er gjort
+    noen endringer før informasjonen skrives til matrisen. Brukeren må få beskjed om at brukeroppføringen
+    er endret. Det er samme krav til validering, feilsjekk og feilmeldinger som i forrige oppgave
+*/
+
+
+// Show userprofile based on an array
+// Show it by using a form
+// Validate, and provide errormessages
+
+
+
+
+
+
+
 require '../sharedViewBottom.php';
