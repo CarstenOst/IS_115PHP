eaa36aafa331c62a7b04afdc23fa976e574458f6
Carsten Østergaard <carsten.ostergaard@sikri.no>
2023-09-20 19:22:12 +0200

    Task 4 sped through

--- Module_4/Task 4/Controller.php
+++ Module_4/Task 4/Controller.php
@@ -1,3 +1,58 @@
 <?php
 require '../sharedViewTop.php';
+
+
+// So here everything is hardcoded, because I spent way to long time on task 2
+$tutoringSessions = [
+    [
+        'title' => 'Mathematics Tutoring',
+        'description' => 'Tutoring in linear algebra and calculus.',
+        'tutoringTime' => '2023-10-05 14:00',
+        'location' => 'Room 101, Main Building'
+    ],
+    [
+        'title' => 'Programming Tutoring',
+        'description' => 'Introduction to Python and data structures.',
+        'tutoringTime' => '2023-10-06 16:00',
+        'location' => 'Room 202, Computer Lab'
+    ],
+    [
+        'title' => 'History Tutoring',
+        'description' => 'Deep dive into European medieval history.',
+        'tutoringTime' => '2023-10-07 13:00',
+        'location' => 'Room 303, History Building'
+    ]
+];
+
+
+
+echo '<div class="w-100">
+      <table class="table mx-auto">
+      <thead class="thead-dark">
+          <tr class="text-left">
+              <th>Title</th>
+              <th>Description</th>
+              <th>Tutoring Time</th>
+              <th>Location</th>
+          </tr>
+      </thead>
+      <tbody>';
+
+foreach ($tutoringSessions as $session) {
+    echo '<tr class="text-left">
+          <td>' . $session['title'] . '</td>
+          <td>' . $session['description'] . '</td>
+          <td>' . $session['tutoringTime'] . '</td>
+          <td>' . $session['location'] . '</td>
+          </tr>';
+}
+
+echo '</tbody>
+      </table>
+      </div>';
+
+
+
+
+
 require '../sharedViewBottom.php';
