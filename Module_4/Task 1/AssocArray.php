cb20098b3b6ccce8ca5ecb651dd4068650430bb6
Carsten Østergaard <carsteno@uia.no>
2023-09-22 07:10:32 +0200

    Comments T1

--- Module_4/Task 1/AssocArray.php
+++ Module_4/Task 1/AssocArray.php
@@ -5,7 +5,8 @@ class AssocArray
     /**
      * Warning: This function will overwrite the variable passed as first parameter
      * Creates an array with keys from the given parameters
-     * @param $arr - array to be created; WARNING WILL OVERWRITE
+     *
+     * @param $arr - array to be created; WARNING WILL OVERWRITE the variable passed as first parameter
      * @param int ...$arrayKey - keys for the array
      * Value will always be 1
      */
@@ -17,34 +18,14 @@ class AssocArray
         }
 
         foreach ($arrayKey as $key) {
-            // Task didn't specify a value, so I just set it to 1
-            $arr[$key] = 1;
+            $arr[$key] = 1; // Task didn't specify a value, so I just set it to 1.
         }
     }
 
-
-    /**
-     * WARNING: THIS MIGHT OVERWRITE THE ARRAY IF KEY IS ALREADY USED
-     * Takes a pointer to the first parameter value, and either adds or overwrites the value (if a key is the same) with the given keys
-     * Be aware of side effects
-     * @param array $arr - array to be modified
-     * @param int ...$arrayKey - keys for the array
-     * @return bool - true if array was modified, false if not
-     */
-    public static function modify(array &$arr, int ...$arrayKey): bool
-    {
-        if (!$arrayKey) {
-            return false;
-        }
-
-        foreach ($arrayKey as $key) {
-            $arr[$key] = 2;
-        }
-        return true;
-    }
 
     /**
      * Prints out an assoc array (both key and value) with the given html code below
+     *
      * @param array $assocArray
      * @return void echos the html code
      */
