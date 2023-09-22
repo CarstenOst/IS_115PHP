8650922fa746c5f7193c48a405596e79404f9b44
Carsten Østergaard <carsten.ostergaard@sikri.no>
2023-09-22 10:19:12 +0200

    Even more comments

--- Module_4/Task 3/Controller.php
+++ Module_4/Task 3/Controller.php
@@ -1,24 +1,32 @@
 <?php
-include 'ChangeHandler.php';
+include 'ChangeHandler.php'; // To handle changes in an array.
+// Shared
 include '../Constants.php';
 include '../SharedHtmlRendererM4.php';
 // Reuse code from task 2.
 include '../Task 2/InputHandler.php';
 include '../Task 2/InputValidation.php';
 
-// This code is very similar to task 2. I moved the new logic used to 'ChangeHandler.php'
-// Now normally session_start() is called before includes, as if they echo anything the code will possibly halt
+// This code is very similar to task 2. I moved the new logic used to 'ChangeHandler.php'.
+// Now normally session_start() is called before includes, as if they echo anything the code will possibly halt.
 // However, I wrote those codes, so I know they won't do that.
 session_start();
 
-// Hardcode a global user in an associative array
+// Hardcode a global user in an associative array.
 $user = [
     NAME_COOKIE => ['Kurt Nilsen', true],
     EMAIL_COOKIE => ['kurt@nilsen.com', true],
     NUMBER_COOKIE => ['47283748', true]
 ];
 
-function renderPageT3($userInput = []): void
+
+/**
+ * Renders the page with the form
+ *
+ * @param array $userInput - optional, if not given, will render the form with empty inputs.
+ * @return void - echos the html code.
+ */
+function renderPageT3(array $userInput = []): void
 {
     include_once '../sharedViewTop.php';
     SharedHtmlRendererM4::renderFormArrayBased(
@@ -28,14 +36,16 @@ function renderPageT3($userInput = []): void
     );
     echo <<<HTML
             <form method="post" action="">
-                <button type="submit" name="deleteSession">Kurt Nilsen</button>
+                <button type="submit" name="deleteSession">Reset (Kurt Nilsen)</button>
             </form>
         HTML;
 }
+
 function processFormT3($userInput): void
 {
     // We want to use the global user.
     global $user;
+
     // If session is empty, we set the oldInput as the hardcoded user (as specified in the task).
     // Else we use the previous sent user stored in session.
     // If the form in task 2 is being used, then session is already set.
@@ -45,7 +55,7 @@ function processFormT3($userInput): void
     }
     $oldInput = $_SESSION;
 
-    // Instantiate the InputHandler class
+    // Instantiate the InputHandler class.
     $handler = new InputHandler();
 
     // Dynamically configure input processing rules.
@@ -57,7 +67,7 @@ function processFormT3($userInput): void
     // Process inputs based on configured rules.
     list($dataInput, $notValidResponseMessage) = $handler->processInputs($userInput);
 
-    // To see if anything was changed or not
+    // To see if anything was changed or not.
     $changes = ChangeHandler::detectChanges($dataInput, $oldInput);
 
     // If errors or not valid response message is not empty, we want to display the errors and the form again.
@@ -68,7 +78,8 @@ function processFormT3($userInput): void
         echo ChangeHandler::changeMsg($changes);
         return;
     }
-    // The task asked for array, and array is given
+
+    // The task asked for array, and array is given.
     $validMessage = [
         "User was successfully registered:",
         "Name: {$dataInput[NAME_COOKIE][0]}",
@@ -76,10 +87,10 @@ function processFormT3($userInput): void
         "Number: {$dataInput[NUMBER_COOKIE][0]}"
     ];
 
-    // Render the page with our new dataInput
+    // Render the page with our new dataInput.
     renderPageT3($dataInput);
 
-    // Echo if there has been any changes or not
+    // Echo if there has been any changes or not.
     echo ChangeHandler::changeMsg($changes);
 
     // Generate a popup response. This will always be true,
@@ -94,15 +105,15 @@ function processFormT3($userInput): void
 // Check if submit is pressed, and process the form if it is. Else check if session is set,
 // and render the page with the session. Lastly if either is true, just render the page with the hardcoded user.
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // If reset is pressed, then destroy the session and insert the standard hard coded user "Kurt Nilsen"
+    // If reset is pressed, then destroy the session and insert the standard hard coded user "Kurt Nilsen".
     if (isset($_POST['deleteSession'])) {
-        session_unset();     // Clear the session data
-        session_destroy();   // Destroy the session
+        session_unset();     // Clear the session data.
+        session_destroy();   // Destroy the session.
 
-        renderPageT3($user); // Render the page with hard coded user
+        renderPageT3($user); // Render the page with hard coded user.
         return;
     }
-    processFormT3($_POST); // If reset is not pressed, then process the form with the input from the user
+    processFormT3($_POST); // If reset is not pressed, then process the form with the input from the user.
 } else if (isset($_SESSION)) {
     renderPageT3([
         NAME_COOKIE => $_SESSION[NAME_COOKIE] ?? '',
