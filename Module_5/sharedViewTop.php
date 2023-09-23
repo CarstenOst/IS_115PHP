1fa1266b1cd50fd67496e7b00e007b4df6f10b8b
Carsten Østergaard <carsten.ostergaard@sikri.no>
2023-09-23 14:17:04 +0200

    + sharedViewTop.php

---
+++ Module_5/sharedViewTop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Include Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <title> <?php require_once 'sharedFunctionsM5.php'; echo sharedFunctionsM5::getCurrentModuleFolderName()?></title>
+</head>
+<body>
+<div class="container d-flex justify-content-center">
+    <div class="flex-column">
+        <div class="d-flex justify-content-center fixed-top">
+            <nav id="navbar" class="d-flex navbar navbar-expand-lg">
+                <?php sharedFunctionsM5::generateNavigationButtons(); ?>
+            </nav>
+        </div>
+        <div id="messageContainer" class="d-flex flex-column align-items-center">
+        </div>
+        <div id="" class="flex-column justify-content-center text-center container">
+            <h2>
+                <?php echo sharedFunctionsm5::generateParentFolderName(); ?>
+            </h2>
