4f0465fe4e573d23f00997765f81c0e6a2c1867e
Carsten Østergaard <carsten.ostergaard@sikri.no>
2023-10-09 10:22:45 +0200

    + ConstantsM5.php

---
+++ Module_5/ConstantsM5.php
@@ -0,0 +1,10 @@
+<?php
+
+const ENCRYPTING = 'Encrypt';
+const DECRYPTING = 'Decrypt';
+
+
+const INPUT_FIELDS_M5T4 = [
+    ENCRYPTING => 'Enter your string to encrypt',
+    DECRYPTING => 'Enter your encrypted string to decrypt'
+];
