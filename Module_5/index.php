301d5a31b0c64da7a5ae3e84355ccd87eebf2a09
Carsten Østergaard <carsteno@uia.no>
2023-11-05 21:34:03 +0100

    hotfix for people using xampp

--- Module_5/index.php
+++ Module_5/index.php
@@ -5,7 +5,7 @@
 // Where __FILE__ is a magic constant that returns the full path and filename of the file it is used in
 // While $_SERVER['SCRIPT_FILENAME'] returns the full path and file name of the script that is currently executed
 // So if they are the same, the statement is true
-if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) {
+if (realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
     header('Location: Task 1/Controller.php');
     exit;
 }
