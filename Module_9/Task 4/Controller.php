626fc48bfaad56d4df9431e360fb292010724c1b
Carsten Østergaard <carsteno@uia.no>
2023-11-30 18:07:20 +0100

    Mod9 bugfix

--- Module_9/Task 4/Controller.php
+++ Module_9/Task 4/Controller.php
@@ -1,18 +1,44 @@
 <?php
 include '../sharedViewTop.php';
 
+/**
+ * Function to download a file
+ *
+ * @param string $filePath
+ * @param string $fileName
+ * @return void downloads the file
+ */
+function downloadFile(string $filePath, string $fileName): void
+{
+    if (file_exists($filePath)) {
+        // Set headers to download the file
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment; filename="' . basename($fileName) . '"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filePath));
 
-if (isset($_GET['download'])) {
-    $file = '../Hidden/mod10.pdf'; // Path to the file to be downloaded
+        // Clean the output buffer
+        ob_end_clean();
 
-    if (file_exists($file)) {
-        header("Content-Type: application/pdf");
-        header("Content-Disposition: attachment; filename=\"mod10.pdf\"");
-        header("Content-Length: " . filesize($file));
-        readfile($file);
+        // Read the file and send it to the output
+        readfile($filePath);
         exit;
     }
+    echo "File not found.";
+
 }
+
+// Check if the download request is set
+if (isset($_GET['download'])) {
+    $filePath = './Hidden/mod10.pdf'; // Absolute path to the file
+    $fileName = 'mod10.pdf'; // Name to send to the client
+
+    downloadFile($filePath, $fileName);
+}
+
 ?>
 
 <a href="Controller.php?download=true">Download pdf
