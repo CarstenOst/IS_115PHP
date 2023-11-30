bc5d492484f96aac63680856d91aeff696d15d31
Carsten Østergaard <carsteno@uia.no>
2023-11-30 17:24:45 +0100

    Mod9 more stuff

--- Module_9/Task 4/Controller.php
+++ Module_9/Task 4/Controller.php
@@ -3,16 +3,16 @@ include '../sharedViewTop.php';
 
 
 if (isset($_GET['download'])) {
-    $file = './Hidden.php'; // Path to the file to be downloaded
+    $file = '../Task 5/Mod9TemplateOutput.pdf'; // Path to the file to be downloaded
 
     if (file_exists($file)) {
         header('Content-Description: File Transfer');
         header('Content-Type: application/pdf');
         header('Content-Disposition: attachment; filename="' . basename($file) . '"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($file));
+        header('Expires: 0'); // Extra stuff
+        header('Cache-Control: must-revalidate'); // Extra stuff
+        header('Pragma: public'); // Extra stuff
+        header('Content-Length: ' . filesize($file)); // Extra stuff
         readfile($file);
         exit;
     }
--- Module_9/Task 5/Controller.php
+++ Module_9/Task 5/Controller.php
@@ -1,6 +1,58 @@
 <?php
+
+use setasign\Fpdi\Fpdi;
+
 include '../sharedViewTop.php';
 
-// TODO finish this task
+// FPDF AND FPDI ARE STOLEN FROM HERE:
+require_once('../fpdf/fpdf.php'); // http://www.fpdf.org/
+require_once('../fpdi/src/autoload.php'); // https://www.setasign.com/products/fpdi/demos/simple-demo/#p-319.5
+
+$pdf = new Fpdi();
+
+$pdf->AddPage();
+
+$pdf->setSourceFile("Mod9Template.pdf");
+
+$fs = $pdf->importPage(1);
+
+$pdf->useTemplate($fs);
+
+$pdf->setFont("Arial", "", 10);
+
+// Logo
+$pdf->Image('Logo.png', 11, 100, 20);
+
+// Set initial X and Y
+$pdf->setY(45);
+$pdf->setX(13);
+
+// First cell with automatic line break
+$pdf->cell(100, 5, "Mike Wazowski", 0, 1, "L");
+
+// Reuse X again as it remains the same
+$pdf->cell(100, 5, "Odderoeyveien 31, 4610 Kristiansand S", 0, 1, "L");
+
+// Move down to Y position 125 for a section under the logo
+$pdf->setY(125);
+$pdf->cell(100, 5, "Mystes AS", 0, 1, "L");
+
+// Position for ticket information
+$pdf->setY(88);
+$pdf->cell(90, 5, "1 stk Young-adult ticket", 0, 0, "L");
+$pdf->cell(90, 5, "199kr", 0, 1, "R");
+
+// Position for the final price
+$pdf->setY(187.5);
+$pdf->cell(105, 5, "199kr", 0, 1, "R");
+
+// Output the PDF
+$pdf->output('F', "Mod9TemplateOutput.pdf");
+
+// It does not look good, but I mean it works...
+echo "<iframe 
+        src=\"Mod9TemplateOutput.pdf\" 
+        style=\"position: absolute; top: 50px; left: 0; width: 100%; height: calc(100% - 50px); border: none;\">
+        </iframe>";
 
 include '../sharedViewBottom.php';
