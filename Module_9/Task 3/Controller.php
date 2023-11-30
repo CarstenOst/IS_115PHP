57800a4b5084f84425900d0f049498d7170da930
Carsten Østergaard <carsteno@uia.no>
2023-11-30 18:20:30 +0100

    Mod9 bugfix

--- Module_9/Task 3/Controller.php
+++ Module_9/Task 3/Controller.php
@@ -1,23 +1,27 @@
 <?php
 include '../sharedViewTop.php';
-echo '<h4> You can try to upload the 123.png in this folder </h4>';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['profileImage'])) {
     $file = $_FILES['profileImage'];
-    $userId = '123'; // The primary key of the user in the "database"
+    $userId = '420'; // The primary key of the user in the "database"
     $uploadDir = '../Task 3/';
     $allowedTypes = ['image/jpeg', 'image/png'];
     $maxSize = 2 * 1024 * 1024; // 2MB
 
     if (!in_array($file['type'], $allowedTypes)) {
-        echo "Only JPG and PNG files are allowed.";
+        echo "Only jpeg and png files are allowed.";
     } elseif ($file['size'] > $maxSize) {
-        echo "It's too big!"; // That's not what she said
+        echo "It's too big!";
     } else {
         $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
         $targetFile = $uploadDir . $userId . '.' . $extension;
         if (move_uploaded_file($file['tmp_name'], $targetFile)) {
-            echo "File was uploaded successfully.";
+            echo "Image was uploaded successfully.";
+            if (file_exists("$userId.png")) {
+                echo "<img src='$userId.png' width='100' height='100'>";
+            } else {
+                echo "<img src='$userId.jpeg' width='100' height='100'>";
+            }
         } else {
             echo "There was an error uploading the file.";
         }
