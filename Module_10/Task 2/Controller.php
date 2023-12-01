3c965ff5a8eb04d91711cd0acbe406e29b464784
Carsten Østergaard <carsteno@uia.no>
2023-12-01 11:45:39 +0100

    mod 10 init

---
+++ Module_10/Task 2/Controller.php
@@ -0,0 +1,23 @@
+<?php
+include '../sharedViewTop.php';
+
+
+function logEvent($message, $logfile = 'log.txt'): void
+{
+    file_put_contents($logfile, date('Y-m-d H:i:s') . ' - '
+        . $message . ' from ip address: ' . $_SERVER['REMOTE_ADDR'] . "\n", FILE_APPEND);
+}
+
+function getLastTenEvents($logfile = 'log.txt'): void
+{
+    $lines = array_slice(file($logfile), -10);
+    foreach ($lines as $line) {
+        echo $line . "<br>";
+    }
+}
+
+// example usage
+logEvent("An request was made to the server");
+getLastTenEvents();
+
+include '../sharedViewBottom.php';
---
+++ Module_10/Task 3/Controller.php
@@ -0,0 +1,4 @@
+<?php
+include '../sharedViewTop.php';
+
+include '../sharedViewBottom.php';
